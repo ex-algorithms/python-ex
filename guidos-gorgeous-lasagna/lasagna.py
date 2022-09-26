6d7174c39931d134264e929fead4fdbe52d9c883
@nullthefirst <ninte.dev@gmail.com>
2022-09-26 11:55:20 +0100

    Removed TODO comments

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -3,14 +3,11 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
 EXPECTED_BAKE_TIME = 40
 
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
+
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -25,22 +22,20 @@ def bake_time_remaining(elapsed_bake_time):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
 def preparation_time_in_minutes(number_of_layers):
     """Calculate the preparation time.
 
     :param number_of_layers: int - number of layers desired.
     :return: int - preparation time (in minutes) derived from 'PREPARATION_TIME'.
 
-    Function that takes the number of layers you want to add to the lasagna as 
+    Function that takes the number of layers you want to add to the lasagna as
     an argument and returns how many minutes you would spend making them
     based on the 'PREPARATION_TIME'.
     """
 
     return PREPARATION_TIME * number_of_layers
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """Calculate the elasped time.
 
@@ -48,8 +43,8 @@ def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     :param elapsed_bake_time: int - baking time already elapsed.
     :return: int - total time (in minutes) derived from 'preparation_time_in_minutes()'.
 
-    Function should return the total number of minutes you've been cooking, or 
-    the sum of your preparation time based on 'preparation_time_in_minutes()', 
+    Function should return the total number of minutes you've been cooking, or
+    the sum of your preparation time based on 'preparation_time_in_minutes()',
     and the time the lasagna has already spent baking in the oven.
     """
 

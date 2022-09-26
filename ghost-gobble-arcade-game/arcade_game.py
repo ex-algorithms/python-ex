222b1397fa0e19b27bf66b62a95b0869657a6f17
@nullthefirst <ninte.dev@gmail.com>
2022-09-26 18:06:01 +0100

    Refactored arcade-game code

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -11,8 +11,8 @@ def eat_ghost(power_pellet_active, touching_ghost):
 
     if power_pellet_active and touching_ghost:
         return True
-    else:
-        return False
+
+    return False
 
 
 def score(touching_power_pellet, touching_dot):
@@ -25,8 +25,8 @@ def score(touching_power_pellet, touching_dot):
 
     if touching_power_pellet or touching_dot:
         return True
-    else:
-        return False
+
+    return False
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -39,10 +39,14 @@ def lose(power_pellet_active, touching_ghost):
 
     if not power_pellet_active and touching_ghost:
         return True
-    elif power_pellet_active and touching_ghost:
+
+    if power_pellet_active and touching_ghost:
         return False
-    elif not touching_ghost:
+
+    if not touching_ghost:
         return False
+
+    return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -56,7 +60,11 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
 
     if has_eaten_all_dots and not power_pellet_active and touching_ghost:
         return False
-    elif not has_eaten_all_dots and power_pellet_active and touching_ghost:
+
+    if not has_eaten_all_dots and power_pellet_active and touching_ghost:
         return False
-    elif has_eaten_all_dots:
+
+    if has_eaten_all_dots:
         return True
+
+    return False

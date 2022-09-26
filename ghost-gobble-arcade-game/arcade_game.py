e56f82dad3ff4cd19d0ed6503f2c035958e783e2
@nullthefirst <ninte.dev@gmail.com>
2022-09-26 17:55:00 +0100

    Solved arcade-game exercise

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -9,7 +9,10 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :return: bool - can the ghost be eaten?
     """
 
-    pass
+    if power_pellet_active and touching_ghost:
+        return True
+    else:
+        return False
 
 
 def score(touching_power_pellet, touching_dot):
@@ -20,7 +23,10 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    pass
+    if touching_power_pellet or touching_dot:
+        return True
+    else:
+        return False
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -31,7 +37,12 @@ def lose(power_pellet_active, touching_ghost):
     :return: bool - has the player lost the game?
     """
 
-    pass
+    if not power_pellet_active and touching_ghost:
+        return True
+    elif power_pellet_active and touching_ghost:
+        return False
+    elif not touching_ghost:
+        return False
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -43,4 +54,9 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    pass
+    if has_eaten_all_dots and not power_pellet_active and touching_ghost:
+        return False
+    elif not has_eaten_all_dots and power_pellet_active and touching_ghost:
+        return False
+    elif has_eaten_all_dots:
+        return True

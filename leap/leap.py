2c2ab563c3d62cba93115d62f6ad2af03e96f86b
@nullthefirst <ninte.dev@gmail.com>
2022-09-27 21:33:14 +0100

    Solved leap exercise

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,24 @@
+"""Determine whether year is a leap year according to the Gregorian calendar."""
+
+
 def leap_year(year):
-    pass
+    """
+    :param year: int - year of choice.
+    :return: bool - is this a leap year?
+    """
+
+    verdict = False
+
+    if year % 4 == 0:
+        verdict = True
+
+    if year % 100 == 0:
+        verdict = False
+
+    if year % 4 == 0 and year % 100 != 0:
+        verdict = True
+
+    if year % 100 == 0 and year % 400 == 0:
+        verdict = True
+
+    return verdict

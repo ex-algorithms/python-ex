405974856426a3d42a9ebbc3ec865cc7a262150c
@nullthefirst <ninte.dev@gmail.com>
2022-09-28 16:13:29 +0100

    Completed equilateral function, added function for zero length sides.

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,36 @@
+"""Identify the type of triangle based on an argument list of sides."""
+
+
+def _zero_length_sides(sides):
+    """Searches triangle sides input for zero.
+
+    :param sides: a list of sides in the triangle to be tested.
+    :return: True, if any side is zero in length.
+    """
+    verdict = False
+
+    for el in sides:
+        if el == 0:
+            verdict = True
+
+    return verdict
+
+
 def equilateral(sides):
-    pass
+    """Identifies whether a triangle is equilateral.
+
+    :param sides: a list of sides in the triangle to be tested.
+    :return: True, if all sides are of equal length.
+    """
+    verdict = False
+
+    if max(sides) == min(sides):
+        verdict = True
+
+    if _zero_length_sides(sides):
+        verdict = False
+
+    return verdict
 
 
 def isosceles(sides):

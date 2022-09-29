57b473bad8053fb9b12138562e09eabae475934f
@nullthefirst <ninte.dev@gmail.com>
2022-09-29 07:37:14 +0100

    Added module and function docstrings, added scaffolding

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,3 +1,5 @@
+""" Determine number of scores in a Yacht dice game - https://en.wikipedia.org/wiki/Yacht_(dice_game). """
+
 # Score categories.
 # Change the values as you see fit.
 YACHT = None
@@ -15,4 +17,17 @@ CHOICE = None
 
 
 def score(dice, category):
-    pass
+    """Calculate the number of scores in the game, based on the selected category.
+
+    :param dice: list - a list of numbers holding dice faces.
+    :param category: bool - boolean contained in a constant variable.
+    :return: int - score based on category selected, zero if category no applicable.
+    """
+
+    dice.sort()
+
+    return dice
+
+
+# scaffolding
+print(score([2, 4, 1, 5, 2], ""))

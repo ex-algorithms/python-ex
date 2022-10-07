e0c1319c831a5d705af3995a58dd297982b6c66f
@nullthefirst <ninte.dev@gmail.com>
2022-10-07 22:10:27 +0100

    Added draft code to collatz exercise

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,20 @@
+def _repeat(num):
+    output = 0
+
+    if num % 2 == 0:
+        output = num / 2
+    else:
+        output = (num * 3) + 1
+
+    return output
+
+
 def steps(number):
-    pass
+    counter = 0
+
+    output = _repeat(number)
+
+    return output
+
+
+print(_repeat(14))

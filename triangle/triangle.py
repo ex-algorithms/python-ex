68c3fd7b7a3f61813c9dcd4c215948b74affd9ef
teyofnull <tey@nullnow.com>
2023-07-11 14:32:56 +0100

    Completed triangle exercise

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,55 @@
+def zero_sides(sides):
+    a, b, c = sides
+
+    if a == 0 and b == 0 and c == 0:
+        return True
+
+
+def valid_triangle(info):
+    x, y, z = info
+
+    if ((x + y) >= z) and ((x + z) >= y) and ((y + z) >= x):
+        return True
+    else:
+        return False
+
+
 def equilateral(sides):
-    pass
+    a, b, c = sides
+
+    if a == b and b == c and a == c:
+        if zero_sides(sides):
+            return False
+        else:
+            return True
+    else:
+        return False
 
 
 def isosceles(sides):
-    pass
+    a, b, c = sides
+
+    if zero_sides(sides):
+        return False
+    else:
+        if a == 1 or b == 1 or c == 1:
+            return False
+        else:
+            if a == b or a == c:
+                return True
+            elif b == c or b == a:
+                return True
+            elif c == a or c == b:
+                return True
+            else:
+                return False
 
 
 def scalene(sides):
-    pass
+    verdict = False
+
+    if valid_triangle(sides):
+        if not isosceles(sides) and not equilateral(sides):
+            verdict = True
+
+    return verdict

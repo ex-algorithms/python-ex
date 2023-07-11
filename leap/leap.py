4af1eeec8430ebc9e2709370da7c01eaa66e051e
teyofnull <tey@nullnow.com>
2023-07-11 11:15:59 +0100

    Completed leap year exercise

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,13 @@
 def leap_year(year):
-    pass
+    verdict = False
+
+    if year % 4 == 0:
+        if year % 100 != 0:
+            verdict = True
+        else:
+            if year % 400 == 0:
+                verdict = True
+            else:
+                verdict = False
+
+    return verdict
